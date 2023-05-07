806b916239830a5f402a42a7b50992dd8a479c04
SerjAyvazyan-2000 <ayvazyanserj3@gmail.com>
2023-05-07 18:47:07 +0400

    setup gh-pages

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import logo from './logo.svg';
+import './App.css';
+import "../src/assets/Fonts/style.css"
+
+function App() {
+  return (
+    <div className="App">
+     <span className="icon-twitter">Hello practice</span>
+    </div>
+  );
+}
+
+export default App;
